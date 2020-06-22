f94c5477adf3d42b707808ee13fb19a536fe755a
Radoslav Valchev <radoslav.vn@gmail.com>
2020-06-22 16:58:05 +0300

    Add js ES6 module import/export; Add pages folder

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+import testModulesUI from './testModulesUI';
+
+;(function(window, document) {
+	// Add your functions here.
+	testModulesUI();
+})(window, document);
---
+++ src/js/testModulesUI.js
@@ -0,0 +1,7 @@
+const testLog = message => {
+	console.log(message);
+}
+
+export default function testModulesUI() {
+	testLog('testLog from module');
+}
