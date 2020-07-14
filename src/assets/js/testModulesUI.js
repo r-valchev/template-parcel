cec19e17bbcb658b79c70a9e8887558da43d19b3
Radoslav Valchev <radoslav.vn@gmail.com>
2020-07-14 14:47:54 +0300

    Add eslint

--- src/assets/js/testModulesUI.js
+++ src/assets/js/testModulesUI.js
@@ -1,6 +1,12 @@
 const testLog = message => {
+	var myVar;
+	myVar = 'test';
+
+	// for (var i = 0; i < 10; i--) {
+	// 	console.log(i);
+	// }
 	console.log(message);
-}
+};
 
 export default function testModulesUI() {
 	testLog('testLog from module');
